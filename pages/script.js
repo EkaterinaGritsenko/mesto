6edf658aeedc181f2df9c7fcaa6083c6f914bf4a
Ekaterina Gritsenko <ekaterinagritsenko@MacBook-Pro-Ekaterina.local>
2023-04-13 23:37:33 +0300

    Закончен js

---
+++ pages/script.js
@@ -0,0 +1,29 @@
+
+let popup = document.querySelector('.popup');
+let profileFullname = document.querySelector('.profile__fullname');
+let profileDescr = document.querySelector('.profile__descr');
+let nameInput = document.querySelector('.input__text_name');
+let jobInput = document.querySelector('.input__text_title');
+let openPopupBnt = document.querySelector('.profile__edit-btn');
+let closePopupBtn = document.querySelector('.popup__close-btn');
+let popupSaveBtn = document.querySelector('.popup__save-btn');
+
+function togglePopupOpen() {
+  popup.classList.toggle('popup_open');
+}
+
+function openPopup() {
+  nameInput.value = profileFullname.innerHTML;
+  jobInput.value = profileDescr.innerHTML;
+  togglePopupOpen();
+}
+
+function closePopupAndSave() {
+  profileFullname.innerHTML = nameInput.value;
+  profileDescr.innerHTML = jobInput.value;
+  togglePopupOpen();
+}
+
+openPopupBnt.addEventListener('click', openPopup);
+closePopupBtn.addEventListener('click', togglePopupOpen);
+popupSaveBtn.addEventListener('click', closePopupAndSave);
