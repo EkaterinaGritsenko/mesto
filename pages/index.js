cd3519e10e9e826e1eeecff8942b14553467700c
Ekaterina Gritsenko <ekaterinagritsenko@MBP-Ekaterina.netis.cc>
2023-05-06 14:05:56 +0300

    реализовано добавление новой карточки в начало

--- pages/index.js
+++ pages/index.js
@@ -26,18 +26,14 @@ const initialCards = [
 ];
 
 const elementTemplate = document.getElementById('element-template');
-
 const elementContainer = document.querySelector('.elements');
 
 const createElement = (cardElement) =>{
-  //console.log(elementTemplate);
   const element = elementTemplate.content.querySelector('.element').cloneNode(true);
-
   const elementImage = element.querySelector('.element__image');
   const elementTitle = element.querySelector('.element__title');
   const elementLikeButton = element.querySelector('.element__like');
   const elementDeleteButton = element.querySelector('.element__delete');
-
   elementTitle.textContent = cardElement.name;
   elementImage.src = cardElement.link;
   elementImage.alt = cardElement.name;
@@ -49,7 +45,6 @@ const createElement = (cardElement) =>{
   const handleLike = (evt) => {
     elementLikeButton.classList.toggle('element__like_active');
   }
-
   elementDeleteButton.addEventListener('click', handleDelete);
   elementLikeButton.addEventListener('click', handleLike);
 
@@ -58,54 +53,92 @@ const createElement = (cardElement) =>{
 
 initialCards.forEach(function(card){
   element = createElement(card);
-  elementContainer.appendChild (element);
-}
+  elementContainer.appendChild(element);
+ }
+);
 
-)
-createElement(initialCards);
 
-// Находим форму в DOM
-let formElement = document.querySelector('.popup__form');// Воспользуйтесь методом querySelector()
-let popup = document.querySelector('.popup');
-let openPopupBnt = document.querySelector('.profile__edit-btn');
-let closePopupBtn = document.querySelector('.popup__close-btn');
-let profileFullname = document.querySelector('.profile__fullname');
-let profileDescr = document.querySelector('.profile__descr');
-let nameInput = document.querySelector('.popup__input_type_name');
+let profileForm = document.querySelector('.popup__form');// Воспользуйтесь методом querySelector() 
+let profilePopup = document.querySelector('.popup-profile'); 
+let elementForm = document.querySelector('.popup-element__form');// Воспользуйтесь методом querySelector() 
+let elementPopup = document.querySelector('.popup-element'); 
+let openPopupBnt = document.querySelector('.profile__edit-btn'); 
+let closePopupBtn = document.querySelector('.popup__close-btn'); 
+let profileFullname = document.querySelector('.profile__fullname'); 
+let profileDescr = document.querySelector('.profile__descr'); 
+let nameInput = document.querySelector('.popup__input_type_name'); 
 let jobInput = document.querySelector('.popup__input_type_job');
+let elementPopupBtn = document.querySelector('.profile__add-btn'); 
 
-function togglePopupOpen() {
-  popup.classList.toggle('popup_opened');
+let closeElementPopupBtn = document.querySelector('.popup-element__close-btn'); 
+let nameElementInput = document.querySelector('.popup-element__input_type_name'); 
+let urlElementInput = document.querySelector('.popup-element__input_type_url');
+function PopupOpen (p) { 
+  p.classList.add('popup_opened'); 
 }
 
-function openPopup() {
-  nameInput.value = profileFullname.textContent;
-  jobInput.value = profileDescr.textContent;
-  togglePopupOpen();
+function PopupClose (p) { 
+  p.classList.remove('popup_opened'); 
 }
 
-openPopupBnt.addEventListener('click', openPopup);
-closePopupBtn.addEventListener('click', togglePopupOpen);
+function openPopupProfile () { 
+  nameInput.value = profileFullname.textContent; 
+  jobInput.value = profileDescr.textContent; 
+  PopupOpen(profilePopup); 
+} 
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function handleFormSubmit (evt) {
-  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
-  // Получите значение полей jobInput и nameInput из свойства value
-  let nameInputValue = nameInput.value;
-  let jobInputValue = jobInput.value;
-  // Выберите элементы, куда должны быть вставлены значения полей
-  let profileFullname = document.querySelector('.profile__fullname');
-  let profileDescr = document.querySelector('.profile__descr');
-  // Вставьте новые значения с помощью textContent
-  profileFullname.textContent = nameInput.value;
-  profileDescr.textContent = jobInput.value;
-  togglePopupOpen();
+function closePopupProfile () { 
+  PopupClose(profilePopup);
+} 
+
+function openPopupElement () {
+  PopupOpen(elementPopup);
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', handleFormSubmit);
+function closePopupElement () {
+  PopupClose(elementPopup);
+}
 
+openPopupBnt.addEventListener('click', openPopupProfile ); 
+closePopupBtn.addEventListener('click', closePopupProfile ); 
+elementPopupBtn.addEventListener('click', openPopupElement) ; 
+closeElementPopupBtn.addEventListener('click', closePopupElement ); 
+
+// Обработчик «отправки» формы, хотя пока 
+// она никуда отправляться не будет 
+function handleFormSubmit (evt) { 
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы. 
+                                                // Так мы можем определить свою логику отправки. 
+                                                // О том, как это делать, расскажем позже. 
+  // Получите значение полей jobInput и nameInput из свойства value 
+  let nameInputValue = nameInput.value; 
+  let jobInputValue = jobInput.value; 
+  // Выберите элементы, куда должны быть вставлены значения полей 
+  let profileFullname = document.querySelector('.profile__fullname'); 
+  let profileDescr = document.querySelector('.profile__descr'); 
+  // Вставьте новые значения с помощью textContent 
+  profileFullname.textContent = nameInput.value; 
+  profileDescr.textContent = jobInput.value; 
+  closePopupProfile(); 
+} 
+
+function handleElementFormSubmit (evt) { 
+  evt.preventDefault();
+  const elementName = document.querySelector('.popup-element__input_type_name').value; 
+  const elementUrl = document.querySelector('.popup-element__input_type_url').value; 
+    const cardForAdd = [
+      {
+        name:elementName,
+        link: elementUrl
+      }];
+      cardForAdd.forEach(function(card){
+        elementCard = createElement(card);
+        elementContainer.insertBefore(elementCard, elementContainer.firstElementChild);
+       }
+      ); 
+  closePopupElement(); 
+}
+// Прикрепляем обработчик к форме: 
+// он будет следить за событием “submit” - «отправка» 
+profileForm.addEventListener('submit', handleFormSubmit); 
+elementForm.addEventListener('submit', handleElementFormSubmit); 
