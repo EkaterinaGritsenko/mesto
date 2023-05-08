6a4733e31b26b8e98204dc3c6302d77d856afe79
Ekaterina Gritsenko <ekaterinagritsenko@MBP-Ekaterina.netis.cc>
2023-05-08 06:38:47 +0300

    реализовано открытие картинки

--- pages/index.js
+++ pages/index.js
@@ -45,23 +45,33 @@ const createElement = (cardElement) =>{
   const handleLike = (evt) => {
     elementLikeButton.classList.toggle('element__like_active');
   }
+
+  const handleImageClick = (evt) => {
+    popupPreview_image.src = elementImage.src;
+    //console.log('elementImage.alt= '+elementImage.alt);
+    //popupPreview_image.alt = elementImage.alt;
+    popupPreview_figcaption.textContent =  elementImage.alt;
+    openPopupPreview();
+  }
+
   elementDeleteButton.addEventListener('click', handleDelete);
   elementLikeButton.addEventListener('click', handleLike);
+  elementImage.addEventListener('click', handleImageClick )
 
   return element;
 }
 
-initialCards.forEach(function(card){
+initialCards.forEach( function(card) {
   element = createElement(card);
   elementContainer.appendChild(element);
  }
 );
 
-
 let profileForm = document.querySelector('.popup__form');// Воспользуйтесь методом querySelector() 
 let profilePopup = document.querySelector('.popup-profile'); 
 let elementForm = document.querySelector('.popup-element__form');// Воспользуйтесь методом querySelector() 
 let elementPopup = document.querySelector('.popup-element'); 
+let previewPopup = document.querySelector('.popup-preview'); 
 let openPopupBnt = document.querySelector('.profile__edit-btn'); 
 let closePopupBtn = document.querySelector('.popup__close-btn'); 
 let profileFullname = document.querySelector('.profile__fullname'); 
@@ -69,40 +79,49 @@ let profileDescr = document.querySelector('.profile__descr');
 let nameInput = document.querySelector('.popup__input_type_name'); 
 let jobInput = document.querySelector('.popup__input_type_job');
 let elementPopupBtn = document.querySelector('.profile__add-btn'); 
-
 let closeElementPopupBtn = document.querySelector('.popup-element__close-btn'); 
 let nameElementInput = document.querySelector('.popup-element__input_type_name'); 
 let urlElementInput = document.querySelector('.popup-element__input_type_url');
-function PopupOpen (p) { 
-  p.classList.add('popup_opened'); 
+let popupPreview_image = document.querySelector('.popup-preview__image'); 
+let popupPreview_figcaption = document.querySelector('.popup-preview__figcaption'); 
+let popupPreview_close_btn = document.querySelector('.popup-preview__close-btn');
+
+function PopupOpen(popupForm) { 
+  popupForm.classList.add('popup_opened'); 
 }
 
-function PopupClose (p) { 
-  p.classList.remove('popup_opened'); 
+function PopupClose(popupForm) { 
+  popupForm.classList.remove('popup_opened'); 
 }
 
-function openPopupProfile () { 
+function openPopupProfile() { 
   nameInput.value = profileFullname.textContent; 
   jobInput.value = profileDescr.textContent; 
   PopupOpen(profilePopup); 
 } 
 
-function closePopupProfile () { 
+function closePopupProfile() { 
   PopupClose(profilePopup);
 } 
 
-function openPopupElement () {
+function openPopupElement() {
+  nameElementInput.value = '';
+  urlElementInput.value = '';
   PopupOpen(elementPopup);
 }
 
-function closePopupElement () {
+function closePopupElement() {
   PopupClose(elementPopup);
 }
 
-openPopupBnt.addEventListener('click', openPopupProfile ); 
-closePopupBtn.addEventListener('click', closePopupProfile ); 
-elementPopupBtn.addEventListener('click', openPopupElement) ; 
-closeElementPopupBtn.addEventListener('click', closePopupElement ); 
+function openPopupPreview() {
+  popupPreview_image.src = popupPreview_image.src; 
+  PopupOpen(previewPopup);
+}
+
+function handlePreviewFormClose() {
+  PopupClose(previewPopup);
+}
 
 // Обработчик «отправки» формы, хотя пока 
 // она никуда отправляться не будет 
@@ -126,19 +145,25 @@ function handleElementFormSubmit (evt) {
   evt.preventDefault();
   const elementName = document.querySelector('.popup-element__input_type_name').value; 
   const elementUrl = document.querySelector('.popup-element__input_type_url').value; 
-    const cardForAdd = [
-      {
-        name:elementName,
-        link: elementUrl
-      }];
-      cardForAdd.forEach(function(card){
-        elementCard = createElement(card);
-        elementContainer.insertBefore(elementCard, elementContainer.firstElementChild);
-       }
-      ); 
+  const cardForAdd = [
+   {
+      name:  elementName,
+      link: elementUrl
+    }];
+    cardForAdd.forEach(function(card){
+      elementCard = createElement(card);
+      elementContainer.insertBefore(elementCard, elementContainer.firstElementChild);
+    }); 
   closePopupElement(); 
 }
+
+openPopupBnt.addEventListener('click', openPopupProfile ); 
+closePopupBtn.addEventListener('click', closePopupProfile ); 
+elementPopupBtn.addEventListener('click', openPopupElement) ; 
+closeElementPopupBtn.addEventListener('click', closePopupElement ); 
+popupPreview_close_btn.addEventListener('click', handlePreviewFormClose); 
 // Прикрепляем обработчик к форме: 
 // он будет следить за событием “submit” - «отправка» 
 profileForm.addEventListener('submit', handleFormSubmit); 
 elementForm.addEventListener('submit', handleElementFormSubmit); 
+
